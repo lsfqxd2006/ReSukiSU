fb9edf375c2a080e960d2a9512413a2eba462fe6
5ec1cff <56485584+5ec1cff@users.noreply.github.com>
2026-04-15 22:51:14 +0800

    kernel: refactor allowlist (https://github.com/tiann/KernelSU/pull/3418)
    - Remove allow_list_arr and allow_list_bitmap
    - Make current_uid as the only key of app profile
    - Use hash table to store allowlist
    - Remove the usage of directly allocating app_profile/root_profile on
    kernel stack
    - Rename `current_uid` to `curr_uid` to avoid ambiguity with kernel
    macro
    
    AlexLiuDev233:
    don't make include and redefine in kernel_compat.h
    We only use them in allowlist.c
    
    [cherry-picked from upstream commit https://github.com/tiann/KernelSU/commit/123040998c4042971a865f0cfe9ec401559a77b1]
    [cherry-picked from xxksu commit https://github.com/backslashxx/KernelSU/commit/f7f803ed845734b4e9267ccaaf815426c8ee7576]
    
    Co-authored-by: AlexLiuDev233 <wzylin11@outlook.com>
    Co-authored-by: backslashxx <118538522+backslashxx@users.noreply.github.com>
    Signed-off-by: backslashxx <118538522+backslashxx@users.noreply.github.com>
    Signed-off-by: AlexLiuDev233 <wzylin11@outlook.com>

--- manager/app/src/main/cpp/jni.c
+++ manager/app/src/main/cpp/jni.c
@@ -123,7 +123,7 @@ NativeBridge(getAppProfile, jobject, jstring pkg, jint uid) {
 	profile.version = KSU_APP_PROFILE_VER;
 
 	strcpy(profile.key, key);
-	profile.current_uid = uid;
+	profile.curr_uid = uid;
 
 	bool useDefaultProfile = get_app_profile(&profile) != 0;
 
@@ -250,7 +250,7 @@ NativeBridge(setAppProfile, jboolean, jobject profile) {
 
 	strcpy(p.key, p_key);
 	p.allow_su = allowSu;
-	p.current_uid = currentUid;
+	p.curr_uid = currentUid;
 
 	if (allowSu) {
 		p.rp_config.use_default = GetEnvironment()->GetBooleanField(env, profile, rootUseDefaultField);
