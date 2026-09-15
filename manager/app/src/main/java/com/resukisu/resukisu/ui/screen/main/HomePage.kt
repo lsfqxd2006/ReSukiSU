1fcb2dabf2ba5cec4ac7283d9fc4d3ea507e034e
lsfqxd2006 <liusongfeng1030@163.com>
2026-09-15 18:01:00 +0800

    Update HomePage.kt

--- manager/app/src/main/java/com/resukisu/resukisu/ui/screen/main/HomePage.kt
+++ manager/app/src/main/java/com/resukisu/resukisu/ui/screen/main/HomePage.kt
@@ -71,6 +71,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
